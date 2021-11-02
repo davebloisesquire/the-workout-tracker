d6c20e89998a5b2e8f947158e542ba7fff690423
theDaveB <davebloisesquire@gmail.com>
2021-11-01 19:39:13 -0700

    server setup, gitignore and package-lock

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+// Required External Package Import
 const express = require("express");
+const mongojs = require("mongojs");
+const logger = require('morgan');
+
+// Database initial setup
+const databaseUrl = "workout-tracker";
+const collections = ["workouts"];
+const db = mongojs(databaseUrl, collections);
+
+// Express instance
+const app = express();
+
+// Middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Logs DB errors to log
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
